df0d2ee65f2578e5f4bf70091143b355a8340b03
Nirav Satani <niravsatani24@gmail.com>
2023-04-20 09:50:14 +0200

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import axios from 'axios'
+
+const JokeTeller = () => {
+  const [keyword, setKeyword] = useState('')
+  const [joke, setJoke] = useState('')
+
+  const handleChange = (event) => {
+    setKeyword(event.target.value)
+  }
+
+  const handleClick = async () => {
+    try {
+      const response = await axios.post('/api/chatgpt3', { keyword })
+      setJoke(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <div className="max-w-3xl mx-auto my-8">
+      <h1 className="text-3xl font-bold text-center">Technical Jokes Teller</h1>
+      <div className="mt-8">
+        <label htmlFor="keyword" className="font-medium text-lg">
+          Enter a keyword:
+        </label>
+        <input
+          id="keyword"
+          type="text"
+          className="w-full mt-2 p-2 rounded border border-gray-300"
+          value={keyword}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="mt-4">
+        <button
+          className="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white font-medium rounded"
+          onClick={handleClick}
+        >
+          Get Joke
+        </button>
+      </div>
+      {joke && (
+        <div className="mt-8 p-4 bg-gray-100 rounded">
+          <p className="font-medium">{joke}</p>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default JokeTeller
